a1bc1c0bb8da98d6dff3d166733c3e2e96b4bf33
himetani <takafumi_t1224@jcom.home.ne.jp>
2018-01-22 23:04:43 +0900

    Add UT for workspace package

--- workspace/workspace_test.go
+++ workspace/workspace_test.go
@@ -2,14 +2,31 @@ package workspace
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
+func setupForInit() error {
+	testHome := os.Getenv("TEST_CONFIGCTL_HOME")
+	if testHome == "" {
+		return errors.New("Set TEST_CONFIGCTL_HOME environment variable to test")
+	}
+	os.Setenv("CONFIGCTL_HOME", testHome)
+	return nil
+}
+
+func teardownForInit() {
+	if err := os.RemoveAll(os.Getenv("CONFIGCTL_HOME")); err != nil {
+		log.Printf("Unexpected error happened at teardown: %s\n", err)
+	}
+}
+
 func TestInit(t *testing.T) {
-	if err := setup(); err != nil {
+	if err := setupForInit(); err != nil {
 		t.Fatal(err)
 	}
 
@@ -19,7 +36,7 @@ func TestInit(t *testing.T) {
 		t.Errorf("Unexpected error happend: %s\n", err)
 	}
 
-	defer teardown()
+	defer teardownForInit()
 
 	if _, err := os.Stat(configCtlHome); os.IsNotExist(err) {
 		t.Errorf("$CONFIGCTL_HOME dir is not created. %s\n", err)
@@ -30,17 +47,76 @@ func TestInit(t *testing.T) {
 	}
 }
 
-func setup() error {
-	testHome := os.Getenv("TEST_CONFIGCTL_HOME")
-	if testHome == "" {
-		return errors.New("Set TEST_CONFIGCTL_HOME environment variable to test")
-	}
-	os.Setenv("CONFIGCTL_HOME", testHome)
-	return nil
-}
+func TestGetConfigs(t *testing.T) {
+	configCtlHome = filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "himetani", "configctl", "workspace", "testdata")
 
-func teardown() {
-	if err := os.RemoveAll(os.Getenv("CONFIGCTL_HOME")); err != nil {
-		log.Printf("Unexpected error happened at teardown: %s\n", err)
-	}
+	/*
+		t.Run("getConfigs", func(t *testing.T) {
+			os.Setenv("CONFIGCTL_HOME", filepath.Join(os.Getenv("GOPATH"), "src", "github.com", "himetani", "configctl", "workspace", "testdata"))
+			type data struct {
+				TestName string
+				Result   []string
+			}
+
+			tests := []data{
+				{"Success", []string{"hoge"}},
+			}
+
+			for i, test := range tests {
+				result := getConfigs()
+				if !reflect.DeepEqual(result, test.Result) {
+					t.Errorf("Test #%d %s: expected '%s', got '%s'", i, test.TestName, test.Result, result)
+				}
+			}
+		})
+	*/
+
+	t.Run("GetConfig", func(t *testing.T) {
+		type data struct {
+			TestName string
+			CfgName  string
+			Err      error
+			Result   Cfg
+		}
+
+		valid := Cfg{
+			Name:       "valid",
+			Hostname:   "hostname",
+			Port:       "port",
+			Abs:        "abs",
+			Username:   "username",
+			PrivateKey: "private_key",
+		}
+
+		tests := []data{
+			{"Success", "valid", nil, valid},
+			{"Noop", "noop", fmt.Errorf("open %s: no such file or directory", filepath.Join(configCtlHome, "configs", "noop", "config.json")), Cfg{}},
+		}
+
+		for i, test := range tests {
+			var result Cfg
+			err := GetConfig(test.CfgName, &result)
+			if err != nil && test.Err == nil {
+				t.Errorf("Unexpected error happend: %s\n", err)
+				continue
+			}
+
+			if err == nil && test.Err != nil {
+				t.Errorf("Unexpected error happend. err expected non-nil, but got nil")
+				continue
+			}
+
+			if test.Err != nil {
+				if err.Error() != test.Err.Error() {
+					t.Errorf("Test #%d %s (Error): expected '%s', got '%s'", i, test.TestName, test.Err, err)
+				}
+				continue
+			}
+
+			if !reflect.DeepEqual(result, test.Result) {
+				t.Errorf("Test #%d %s: expected '%+v', got '%+v'", i, test.TestName, test.Result, result)
+			}
+		}
+
+	})
 }
